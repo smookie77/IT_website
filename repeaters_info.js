492a8d2acd3017b1528156cd86895576653c843b
MrGrigorov <aleksandar.a.grigorov.2023@elsys-bg.org>
2024-05-15 14:07:37 +0300

    Added js in the html code

---
+++ repeaters_info.js
@@ -0,0 +1,9 @@
+function showMapPage() {
+    document.getElementById("map").style.display = "block";
+    document.getElementById("info").style.display = "none";
+  }
+  
+  function showInformationPage() {
+    document.getElementById("map").style.display = "none";
+    document.getElementById("info").style.display = "block";
+  }
