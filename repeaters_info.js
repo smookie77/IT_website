2ad28117c4bac9e14bfe462bd6be353530cfb179
MrGrigorov <aleksandar.a.grigorov.2023@elsys-bg.org>
2024-05-19 15:41:39 +0300

    Combined two js files

--- repeaters_info.js
+++
@@ -1,9 +0,0 @@
-function showMapPage() {
-    document.getElementById("map").style.display = "block";
-    document.getElementById("info").style.display = "none";
-  }
-  
-  function showInformationPage() {
-    document.getElementById("map").style.display = "none";
-    document.getElementById("info").style.display = "block";
-  }
--- script.js
+++ script.js
@@ -96,3 +96,12 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById('navbar').innerHTML = data;
     });
 });
+function showMapPage() {
+  document.getElementById("map").style.display = "block";
+  document.getElementById("info").style.display = "none";
+}
+
+function showInformationPage() {
+  document.getElementById("map").style.display = "none";
+  document.getElementById("info").style.display = "block";
+}
